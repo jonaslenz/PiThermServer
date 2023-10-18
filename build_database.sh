fe48256abaf9edc63905c4c5590a4e37721d1d94
jonaslenz <38277721+jonaslenz@users.noreply.github.com>
2023-10-18 23:19:49 +0200

    Add possible to hold multiple sensors

--- build_database.sh
+++ build_database.sh
@@ -4,5 +4,18 @@
 #
 # Tom Holderness 22/01/2012
 sqlite3 piTemps.db 'DROP TABLE temperature_records;'
-sqlite3 piTemps.db 'CREATE TABLE temperature_records(unix_time bigint primary key, celsius real);' 
+sqlite3 piTemps.db 'CREATE TABLE temperature_records(
+                        sensno int,
+                        unix_time bigint,
+                        celsius real,
+                        PRIMARY KEY(sensno, unix_time)
+                        );' 
+sqlite3 piTemps.db 'DROP TABLE sensors;'
+
+sqlite3 piTemps.db 'CREATE TABLE sensors(
+                        sensno int,
+                        sens_description TEXT,
+                        sensid TEXT,
+                        PRIMARY KEY(sensno)
+                        );' 
 
